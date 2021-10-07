b3c4a19a2f6b6cb0864214a60c836dede69578e2
kubanamaxa2 <jakubniemczyk09@gmail.com>
2021-10-07 13:19:10 +0200

    4

--- app/src/main/java/com/example/kalkulatormobilne/MainActivity.kt
+++ app/src/main/java/com/example/kalkulatormobilne/MainActivity.kt
@@ -6,10 +6,14 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        var wynik = 0;
+        var napis = wynik.toString()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
@@ -17,6 +21,36 @@ class MainActivity : AppCompatActivity() {
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+        findViewById<Button>(R.id.btn1).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "1"
+        }
+        findViewById<Button>(R.id.btn2).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "2"
+        }
+        findViewById<Button>(R.id.btn3).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "3"
+        }
+        findViewById<Button>(R.id.btn4).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "4"
+        }
+        findViewById<Button>(R.id.btn5).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "5"
+        }
+        findViewById<Button>(R.id.btn6).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "6"
+        }
+        findViewById<Button>(R.id.btn7).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "7"
+        }
+        findViewById<Button>(R.id.btn8).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "8"
+        }
+        findViewById<Button>(R.id.btn9).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "9"
+        }
+        findViewById<Button>(R.id.btn10).setOnClickListener{
+            findViewById<Button>(R.id.wyniktext).text = "0"
         }
     }
 
